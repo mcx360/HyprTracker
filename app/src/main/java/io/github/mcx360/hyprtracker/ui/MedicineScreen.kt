2818e9ca62a8ebeb7eb9be9a6275670d51378f96
mkornam <mkornacki888@gmail.com>
2026-03-08 13:58:42 +0000

    added empty screen content for medicine screen

--- app/src/main/java/io/github/mcx360/hyprtracker/ui/MedicineScreen.kt
+++ app/src/main/java/io/github/mcx360/hyprtracker/ui/MedicineScreen.kt
@@ -1,13 +1,26 @@
 package io.github.mcx360.hyprtracker.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import io.github.mcx360.hyprtracker.R
 
 @Composable
 fun MedicineScreen(modifier: Modifier = Modifier){
@@ -16,6 +29,22 @@ fun MedicineScreen(modifier: Modifier = Modifier){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(io.github.mcx360.hyprtracker.R.string.medicine_screen_label))
+        Image(
+            painter = painterResource(R.drawable.empty_medicine_screen_image),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.Empty_Medicine_Screen_Title),
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.Empty_Medicine_Screen_Text),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(all = 16.dp))
     }
 }
