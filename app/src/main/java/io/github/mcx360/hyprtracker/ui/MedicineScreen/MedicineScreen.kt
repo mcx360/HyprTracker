673a115b43d7eaa4600e6bd1a63a59c21a39810f
mkornam <mkornacki888@gmail.com>
2026-03-25 22:02:10 +0000

    worked a lil on medicine screen

--- app/src/main/java/io/github/mcx360/hyprtracker/ui/MedicineScreen/MedicineScreen.kt
+++ app/src/main/java/io/github/mcx360/hyprtracker/ui/MedicineScreen/MedicineScreen.kt
@@ -7,17 +7,30 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,20 +46,92 @@ fun MedicineScreen(
     modifier: Modifier = Modifier,
     openAddMedicationScreen: MutableState<Boolean>
 ){
+    var dropDownMenuExpanded by remember { mutableStateOf(false) }
+    var sliderState = r
+
     if (openAddMedicationScreen.value){
         Column(modifier = modifier
             .fillMaxSize()
             .padding(8.dp),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Card() {
-                Column(modifier = modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8 .dp)) {
-                    Text("Add Medication", textAlign = TextAlign.Left)
+                Column(modifier = modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                    Text(
+                        text = "Medication",
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = modifier.padding(4.dp),
+                        fontWeight = FontWeight.Bold,
+                    )
+
+                    OutlinedTextField(
+                        onValueChange = {},
+                        value = "",
+                        label = {Text("Medication name")}
+                    )
+
+                    OutlinedTextField(
+                        onValueChange = {},
+                        value = "",
+                        label = {Text("Medication description")}
+                    )
+
+                    OutlinedTextField(
+                        onValueChange = {},
+                        value = "",
+                        label = {Text("Medication dosage")}
+                    )
                 }
             }
-            Button(onClick = {openAddMedicationScreen.value = !openAddMedicationScreen.value}) {
-                Text("Add medication")
+
+            Spacer(modifier = modifier.height(16.dp))
+
+            Card() {
+                Column(modifier = modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Reminders",
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = modifier.padding(4.dp),
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Row() {
+                        Text("Reminders daily: 2")
+
+                        IconButton(onClick = {}) {
+                            Image(Icons.Filled.Edit, contentDescription = null)
+                        }
+                    }
+
+                    Row() {
+                        Button(onClick = {}) {
+                            Icon(painter = painterResource(R.drawable.ic_date), contentDescription = null)
+                            Text("12:00")
+                        }
+                        Button(onClick = {}) {
+                            Icon(painter = painterResource(R.drawable.ic_date), contentDescription = null)
+                            Text("20:00")
+                        }
+                    }
+                }
+            }
+
+
+            Row() {
+                Button(onClick = {openAddMedicationScreen.value = !openAddMedicationScreen.value}, modifier = Modifier.weight(1f).padding(16.dp)) {
+                    Text("Cancel")
+                }
+                Button(onClick = {openAddMedicationScreen.value = !openAddMedicationScreen.value}, modifier = Modifier.weight(1f).padding(16.dp)) {
+                    Text("Add medication")
+                }
             }
         }
 
