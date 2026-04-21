4d5b86c3faacc06d022ed8c7d5f7404f00a06f92
mkornam <mkornacki888@gmail.com>
2026-04-21 17:22:42 +0100

    enhanced filter cards ui

--- app/src/main/java/io/github/mcx360/hyprtracker/ui/graphScreen/components/InfoCards.kt
+++ app/src/main/java/io/github/mcx360/hyprtracker/ui/graphScreen/components/InfoCards.kt
@@ -4,15 +4,23 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import io.github.mcx360.hyprtracker.R
 import java.time.LocalDate
 
 @Composable
@@ -46,63 +54,197 @@ fun InfoCards(
             } })
         ) {
             Column(
-                modifier = modifier.fillMaxWidth(),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                 verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.Start
             ) {
-                Text("Systolic", style = MaterialTheme.typography.titleLarge)
-                Text(when(filterOption){
-                    "All time" -> if (systolicDataShown == "Average") getSystolicAverage(null).toString() else if (systolicDataShown == "Max") getSystolicMax(null).toString() else getSystolicMin(null).toString()
-                    "Month" -> if (systolicDataShown == "Average") getSystolicAverage(LocalDate.now().minusMonths(1).toString()).toString() else if (systolicDataShown == "Max") getSystolicMax(LocalDate.now().minusMonths(1).toString()).toString() else getSystolicMin(LocalDate.now().minusMonths(1).toString()).toString()
-                    else -> if (systolicDataShown == "Average") getSystolicAverage(LocalDate.now().minusWeeks(1).toString()).toString() else if (systolicDataShown == "Max") getSystolicMax(LocalDate.now().minusWeeks(1).toString()).toString() else getSystolicMin(LocalDate.now().minusWeeks(1).toString()).toString()
-                })
+
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = modifier.fillMaxWidth()
+                ) {
+                    Text("Systolic", textAlign = TextAlign.Start)
+                    Spacer(modifier.weight(1f))
+                    Icon(
+                        painter = painterResource(R.drawable.heart_3_),
+                        contentDescription = null,
+                    )
+                }
+
+                Row {
+                    Text(
+                        when (filterOption) {
+                            "All time" -> when (systolicDataShown) {
+                                "Average" -> getSystolicAverage(null)
+                                "Max" -> getSystolicMax(null)
+                                else -> getSystolicMin(null)
+                            }
+
+                            "Month" -> when (systolicDataShown) {
+                                "Average" -> getSystolicAverage(LocalDate.now().minusMonths(1).toString())
+                                "Max" -> getSystolicMax(LocalDate.now().minusMonths(1).toString())
+                                else -> getSystolicMin(LocalDate.now().minusMonths(1).toString())
+                            }
+
+                            else -> when (systolicDataShown) {"Average" -> getSystolicAverage(LocalDate.now().minusWeeks(1).toString())
+                                "Max" -> getSystolicMax(LocalDate.now().minusWeeks(1).toString())
+                                else -> getSystolicMin(LocalDate.now().minusWeeks(1).toString())
+                            }
+                        },
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "mmHg",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier
+                            .align(Alignment.Bottom)
+                            .padding(horizontal = 8.dp)
+                        )
+                }
                 Text(systolicDataShown)
             }
         }
 
         //Diastolic Info
-        Card(modifier = modifier.weight(0.33f).padding(horizontal = 8.dp).clickable(onClick = {
-            when (diastolicDataShown) {
-                "Average" -> updateDiastolicDataShown("Max")
-                "Max" -> updateDiastolicDataShown("Min")
-                else -> updateDiastolicDataShown("Average")
-            } })
+        Card(
+            modifier = modifier
+                .weight(0.33f)
+                .padding(horizontal = 8.dp)
+                .clickable(onClick = {
+                    when (diastolicDataShown) {
+                        "Average" -> updateDiastolicDataShown("Max")
+                        "Max" -> updateDiastolicDataShown("Min")
+                        else -> updateDiastolicDataShown("Average")
+                    }
+                })
         ) {
             Column(
-                modifier = modifier.fillMaxWidth(),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                 verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.Start
             ) {
-                Text("Diastolic", style = MaterialTheme.typography.titleLarge)
-                Text(when(filterOption){
-                    "All time" -> if (diastolicDataShown == "Average") getDiastolicAverage(null) else if (diastolicDataShown == "Max") getDiastolicMax(null) else getDiastolicMin(null)
-                    "Month" -> if (diastolicDataShown == "Average") getDiastolicAverage(LocalDate.now().minusMonths(1).toString()) else if (diastolicDataShown == "Max") getDiastolicMax(LocalDate.now().minusMonths(1).toString()) else getDiastolicMin(LocalDate.now().minusMonths(1).toString())
-                    else -> if (diastolicDataShown == "Average") getDiastolicAverage(LocalDate.now().minusWeeks(1).toString()) else if (diastolicDataShown == "Max") getDiastolicMax(LocalDate.now().minusWeeks(1).toString()) else getDiastolicMin(LocalDate.now().minusWeeks(1).toString())
-                })
-                Text(diastolicDataShown)
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = modifier.fillMaxWidth()
+                ){
+                    Text("Diastolic")
+                    Spacer(modifier.weight(1f))
+                    Icon(
+                        painter = painterResource(R.drawable.heart_3_),
+                        contentDescription = null,
+                    )
+                }
+                Row {
+                    Text(
+                        when (filterOption) {
+                            "All time" -> when (diastolicDataShown) {
+                                "Average" -> getDiastolicAverage(null)
+                                "Max" -> getDiastolicMax(null)
+                                else -> getDiastolicMin(null)
+                            }
+                            "Month" -> when (diastolicDataShown) {
+                                "Average" -> getDiastolicAverage(LocalDate.now().minusMonths(1).toString())
+                                "Max" -> getDiastolicMax(LocalDate.now().minusMonths(1).toString())
+                                else -> getDiastolicMin(LocalDate.now().minusMonths(1).toString())
+                            }
+                            else -> when (diastolicDataShown) {
+                                "Average" -> getDiastolicAverage(LocalDate.now().minusWeeks(1).toString())
+                                "Max" -> getDiastolicMax(LocalDate.now().minusWeeks(1).toString())
+                                else -> getDiastolicMin(LocalDate.now().minusWeeks(1).toString())
+                            }
+                        },
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "mmHg",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier
+                            .align(Alignment.Bottom)
+                            .padding(horizontal = 8.dp)
+                    )
+                }
+                Text(
+                    diastolicDataShown,
+                    modifier = modifier.fillMaxWidth(),
+                )
             }
-
         }
 
         //Pulse info
-        Card(modifier = modifier.weight(0.33f).clickable(onClick = {
-            when (pulseDataShown) {
-                "Average" -> updatePulseDataShown("Max")
-                "Max" -> updatePulseDataShown("Min")
-                else -> updatePulseDataShown("Average")
-            } })
+        Card(
+            modifier = modifier
+                .weight(0.33f)
+                .clickable(onClick = {
+                    when (pulseDataShown) {
+                        "Average" -> updatePulseDataShown("Max")
+                        "Max" -> updatePulseDataShown("Min")
+                        else -> updatePulseDataShown("Average")
+                    }
+                })
         ) {
             Column(
-                modifier = modifier.fillMaxWidth(),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                 verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.Start
             ) {
-                Text("Pulse", style = MaterialTheme.typography.titleLarge)
-                Text(when(filterOption){
-                    "All time" -> if (pulseDataShown == "Average") getPulseAverage(null) else if (pulseDataShown == "Max") getPulseMax(null) else getPulseMin(null)
-                    "Month" -> if (pulseDataShown == "Average") getPulseAverage(LocalDate.now().minusMonths(1).toString()) else if (diastolicDataShown == "Max") getPulseMax(LocalDate.now().minusMonths(1).toString()) else getPulseMin(LocalDate.now().minusMonths(1).toString())
-                    else -> if (pulseDataShown == "Average") getPulseAverage(LocalDate.now().minusWeeks(1).toString()) else if (pulseDataShown == "Max") getPulseMax(LocalDate.now().minusWeeks(1).toString()) else getPulseMin(LocalDate.now().minusWeeks(1).toString())
-                })
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Pulse")
+                    Spacer(modifier.weight(1f))
+                    Icon(
+                        painter = painterResource(R.drawable.activity_1_),
+                        contentDescription = null,
+                    )
+                }
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        when (filterOption) {
+                            "All time" -> when (pulseDataShown) {
+                                "Average" -> getPulseAverage(null)
+                                "Max" -> getPulseMax(null)
+                                else -> getPulseMin(null)
+                            }
+
+                            "Month" -> when (pulseDataShown) {
+                                "Average" -> getPulseAverage(LocalDate.now().minusMonths(1).toString())
+                                "Max" -> getPulseMax(LocalDate.now().minusMonths(1).toString())
+                                else -> getPulseMin(LocalDate.now().minusMonths(1).toString())
+                            }
+
+                            else -> when (pulseDataShown) {
+                                "Average" -> getPulseAverage(LocalDate.now().minusWeeks(1).toString())
+                                "Max" -> getPulseMax(LocalDate.now().minusWeeks(1).toString())
+                                else -> getPulseMin(LocalDate.now().minusWeeks(1).toString())
+                            }
+                        },
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold
+
+                    )
+                    Text(
+                        text = "bpm",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier
+                            .align(Alignment.Bottom)
+                            .padding(horizontal = 8.dp)
+                    )
+                }
                 Text(pulseDataShown)
             }
         }
