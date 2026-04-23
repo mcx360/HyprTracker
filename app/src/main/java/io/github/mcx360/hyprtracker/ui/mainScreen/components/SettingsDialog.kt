f372f4fc11605da745de2de37b4c3878ba1d9e2d
mkornam <mkornacki888@gmail.com>
2026-04-23 14:40:35 +0100

    started working on settings

--- app/src/main/java/io/github/mcx360/hyprtracker/ui/mainScreen/components/SettingsDialog.kt
+++ app/src/main/java/io/github/mcx360/hyprtracker/ui/mainScreen/components/SettingsDialog.kt
@@ -23,10 +23,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import io.github.mcx360.hyprtracker.R
 
 @Composable
 fun Settings(
@@ -68,7 +70,42 @@ fun Settings(
                         .verticalScroll(rememberScrollState())
                         .padding(16.dp)
                 ) {
+                    Text("General", style = MaterialTheme.typography.headlineMedium)
+                    Text("Theme", fontWeight = FontWeight.Bold)
+                    Text("System Default", style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,)
+                    Spacer(modifier = Modifier.padding(vertical = 8.dp))
+                    Text("Language", fontWeight = FontWeight.Bold)
+                    Text("English (UK)", style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,)
+                    Spacer(modifier = Modifier.padding(vertical = 8.dp))
+                    Text("Classification table", fontWeight = FontWeight.Bold)
+                    Text("Internation society of hypertension", style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,)
 
+                    Text("Data", style = MaterialTheme.typography.headlineMedium, modifier = Modifier.padding(top = 16.dp))
+                    Text("Delete BP data", fontWeight = FontWeight.Bold)
+                    Text("Permanently delete all bp data", style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,)
+                    Spacer(modifier = Modifier.padding(vertical = 8.dp))
+
+                    Text("Delete medication data", fontWeight = FontWeight.Bold)
+                    Text("Permanently delete all medication data", style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,)
+
+                    Text("About", style = MaterialTheme.typography.headlineMedium, modifier = Modifier.padding(top = 16.dp))
+                    Text("Version", fontWeight = FontWeight.Bold)
+                    Text("Version 0.5.0", style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,)
+                    Spacer(modifier = Modifier.padding(vertical = 8.dp))
+                    Text("Help", fontWeight = FontWeight.Bold)
+                    Text("FAQ help", style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,)
+                    Spacer(modifier = Modifier.padding(vertical = 8.dp))
+                    //Text("3rd party licenses")
+                    Icon(painter = painterResource(R.drawable.ic_about), contentDescription = null, modifier = Modifier.padding(vertical = 16.dp))
+                    Text("HyprTracker is a mobile application designed to help users conveniently record and track their blood pressure readings. It does not provide medical advice, diagnosis, or treatment. Always consult a qualified healthcare professional regarding any medical concerns or before making decisions about your health.",style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,)
                 }
             }
         }
