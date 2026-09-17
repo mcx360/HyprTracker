535933921d223dedfb2e70726090ebe9a043f406
mkornam <mkornacki888@gmail.com>
2026-09-17 15:47:36 +0100

    fixed system ui colours showing wrong colours

--- app/src/main/java/io/github/mcx360/hyprtracker/ui/MainActivity.kt
+++ app/src/main/java/io/github/mcx360/hyprtracker/ui/MainActivity.kt
@@ -2,12 +2,14 @@ package io.github.mcx360.hyprtracker.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalResources
 import io.github.mcx360.hyprtracker.ui.mainScreen.HyprTrackerScreen
 import io.github.mcx360.hyprtracker.ui.mainScreen.settings.ThemeViewModel
 import io.github.mcx360.hyprtracker.ui.theme.AppTheme
@@ -17,9 +19,9 @@ class MainActivity : ComponentActivity() {
 
     private val themeViewModel: ThemeViewModel by viewModels()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             val themeMode by themeViewModel.themeMode.collectAsState()
 
@@ -30,7 +32,14 @@ class MainActivity : ComponentActivity() {
             }
 
             AppTheme(darkTheme = darkTheme) {
-                HyprTrackerScreen(themeViewModel = themeViewModel)
+                if (darkTheme){
+                    enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(414141))
+                    HyprTrackerScreen(themeViewModel = themeViewModel)
+                }
+                else{
+                    enableEdgeToEdge(statusBarStyle = SystemBarStyle.light(414141, 414141))
+                    HyprTrackerScreen(themeViewModel = themeViewModel)
+                }
             }
         }
     }
